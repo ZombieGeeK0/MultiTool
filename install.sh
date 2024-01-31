b5990b2cabc2a6eddb5fa38f646ed0b76be0d758
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-01-31 16:07:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,15 @@ clear
 echo --------------------------------------------------------------------------------------
 echo [~] Instalando...
 echo --------------------------------------------------------------------------------------
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-pip
+pip3 install os
+pip3 install colarama
+pip3 install socket
+pip3 install sys
+git clone https://github.com/sherlock-project/sherlock.git
+cd sherlock
+python3 -m pip install -r requirements.txt
+echo --------------------------------------------------------------------------------------------
+echo [~] Requerimientos instalados. Usa el comando python3 main.py para inciar MultiTool.

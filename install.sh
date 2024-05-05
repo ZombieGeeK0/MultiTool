f2a7d1029240b76b625e6c37159eab66acb13439
Phoenix <158185295+ZombieGeeK0@users.noreply.github.com>
2024-05-05 12:41:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,33 @@
-sudo su
-clear
-echo --------------------------------------------------------------------------------------
-echo [~] Instalando... (Espero que tengas instalado metaexploit)
-echo --------------------------------------------------------------------------------------
-sudo apt-get update
-sudo apt-get install python3
-sudo apt-get install python3-pip
-pip3 install os
-pip3 install colarama
-pip3 install socket
-pip3 install sys
-sudo apt-get install crunch
-sudo apt-get install hydra
-sudo apt-get install Netcat
-echo --------------------------------------------------------------------------------------------
-echo [~] Requerimientos instalados. Usa el comando python3 main.py para inciar MultiTool.
+function install() {
+   clear
+   echo -e "\033[32m[~] Updating packets..."
+   sudo apt update -y && sudo apt upgrade -y
+  
+
+   sleep 3
+   which python3 > /dev/null 2>&1
+   if [ "$?" -eq "0" ]; then
+   echo -e "\033[32m\n[~] Python3 is installed."
+   else
+   echo -e "\033[31m\n[!] Python3 isn´t installer."
+   sleep 2
+   echo -e "\033[32m\n[~] Installing python3..."
+   sudo apt install python3 -y
+   fi
+   
+   sleep 3
+   which pip3 > /dev/null 2>&1
+   if [ "$?" -eq "0" ]; then
+   echo -e "\033[32m\n[~] pip3 is installed."
+   else
+   echo -e "\033[31m\n[!] pip3 isn't installed."
+   sleep 2
+   echo -e "\033[32m\n[~] Installing pip3..."
+   sudo apt install python3-pip -y
+   fi
+   
+   echo -e "\033[32m\n[~] Installing requirements..."
+   pip3 install -r requirements.txt
+}
+
+install
